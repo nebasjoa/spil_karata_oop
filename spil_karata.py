de22834d951ddc44d8de1a898ed96e68d4383aa1
= <nebasjoa@gmail.com>
2020-08-12 21:49:35 +0200

    talon i deljenje fix

--- spil_karata.py
+++ spil_karata.py
@@ -23,11 +23,19 @@ class Spil():
         a = random.randint(4, 40)
         deo_za_talon = self.karte[0:a]
         deo_za_deljenje = self.karte[a:]
+        karte_i1 = []
+        karte_i2 = []
         talon = []
         talon.append(random.sample(deo_za_talon, 4))
-        print(talon)
+        karte_i1.append(random.sample(deo_za_deljenje, 6))
+        deo_za_deljenje = [x for x in deo_za_deljenje if x not in karte_i1[0]]
+        karte_i2.append(random.sample(deo_za_deljenje, 6))
+        deo_za_deljenje = [x for x in deo_za_deljenje if x not in karte_i2[0]]
+        deo_za_talon = [x for x in deo_za_talon if x not in talon[0]]
+        ostale_karte = deo_za_deljenje + deo_za_talon
 
 
-novi_spil = Spil()
-novi_spil.podeli()
+if __name__ == "__main__":
+    novi_spil = Spil()
+    novi_spil.podeli()
 
